77e6bbd40be1e48cd9212e959696cb113306602a
freyssinier <encuncloide@gmail.com>
2026-03-20 21:28:49 -0600

    Remove venv from repo

--- venv/Lib/site-packages/prefect/server/ui-v2/assets/block-type-logo-CwAVRtST.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./vendor-tanstack-DE4ALVGD.js";import{cD as a}from"./index-BZ-K-C9c.js";const c=({alt:o,logoUrl:s,size:r="sm"})=>e.jsx("img",{alt:o,src:s??void 0,className:i({size:r})}),i=a("border border-border bg-muted rounded",{variants:{size:{sm:"size-8",lg:"size-14"}}});export{c as B};
-//# sourceMappingURL=block-type-logo-CwAVRtST.js.map
--- venv/Lib/site-packages/prefect/server/ui-v2/assets/cronstrue-Dgg2IuMf.js
+++
@@ -1,2 +0,0 @@
-import{g as r}from"./vendor-react-HBv6Kpyp.js";import{a1 as o}from"./vendor-date-Bck8LDfw.js";var t=o();const a=r(t);export{a as c};
-//# sourceMappingURL=cronstrue-Dgg2IuMf.js.map
